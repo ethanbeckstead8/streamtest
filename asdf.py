d654e1a9ab56221fc4c2db85dbadebe4bb7555ec
Ethan Beckstead <96321331+ethanbeckstead8@users.noreply.github.com>
2024-12-17 22:42:34 -0700

    Update asdf.py

--- asdf.py
+++ asdf.py
@@ -4,3 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 st.title('hello')
 st.write('hi')
+df = pd.read_csv('nfl_package/datasets/kc_offensive_game_stats_cleaned.csv')
+#df2 = pd.read_csv('nfl_package/datasets/kc_opp_game_stats_cleaned.csv')
+#df3 = pd.read_csv('nfl_package/datasets/min_offensive_game_stats_cleaned.csv')
+#df4 = pd.read_csv('nfl_package/datasets/min_opp_game_stats_cleaned.csv')
