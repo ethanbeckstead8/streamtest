16bfc21155ec14fbe1025711c0dbd04eebe11335
Ethan Beckstead <96321331+ethanbeckstead8@users.noreply.github.com>
2024-12-17 22:51:17 -0700

    Update asdf.py

--- asdf.py
+++ asdf.py
@@ -6,7 +6,7 @@ st.write('hi')
 offK = pd.read_csv('nfl_package/datasets/kc_offensive_game_stats_cleaned.csv')
 defK = pd.read_csv('nfl_package/datasets/kc_opp_game_stats_cleaned.csv')
 offM = pd.read_csv('nfl_package/datasets/min_offensive_game_stats_cleaned.csv')
-offD = pd.read_csv('nfl_package/datasets/min_opp_game_stats_cleaned.csv')
+defM = pd.read_csv('nfl_package/datasets/min_opp_game_stats_cleaned.csv')
 offK['team'] = 'Kansas City'
 offM['team'] = 'Minnesota'
 
