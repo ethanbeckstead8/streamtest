3aad48111ba0df9c528afdc044dc443db3de9f4a
Ethan Beckstead <96321331+ethanbeckstead8@users.noreply.github.com>
2024-12-17 22:49:50 -0700

    Update asdf.py

--- asdf.py
+++ asdf.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 st.title('hello')
@@ -8,5 +7,16 @@ offK = pd.read_csv('nfl_package/datasets/kc_offensive_game_stats_cleaned.csv')
 defK = pd.read_csv('nfl_package/datasets/kc_opp_game_stats_cleaned.csv')
 offM = pd.read_csv('nfl_package/datasets/min_offensive_game_stats_cleaned.csv')
 offD = pd.read_csv('nfl_package/datasets/min_opp_game_stats_cleaned.csv')
+offK['team'] = 'Kansas City'
+offM['team'] = 'Minnesota'
+
+offense = pd.concat([offK, offM], ignore_index=True)
+offense.reset_index(drop=True, inplace=True)
+defense = pd.concat([defK, defM], ignore_index=True)
+defense.reset_index(drop=True, inplace=True)
 st.sidebar.header("Filter Data Here")
 tab1, tab2 = st.tabs(["Offense", "Defense"])
+with tab 1:
+  st.write(offense)
+with tab 2:
+  st.write(defense)
