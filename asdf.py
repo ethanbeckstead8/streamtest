96e70f361b713b3f9541f0fc8266230202699103
Ethan Beckstead <96321331+ethanbeckstead8@users.noreply.github.com>
2024-12-17 22:45:02 -0700

    Update asdf.py

--- asdf.py
+++ asdf.py
@@ -4,9 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 st.title('hello')
 st.write('hi')
-df = pd.read_csv('nfl_package/datasets/kc_offensive_game_stats_cleaned.csv')
-df2 = pd.read_csv('nfl_package/datasets/kc_opp_game_stats_cleaned.csv')
-df3 = pd.read_csv('nfl_package/datasets/min_offensive_game_stats_cleaned.csv')
-df4 = pd.read_csv('nfl_package/datasets/min_opp_game_stats_cleaned.csv')
+offK = pd.read_csv('nfl_package/datasets/kc_offensive_game_stats_cleaned.csv')
+defK = pd.read_csv('nfl_package/datasets/kc_opp_game_stats_cleaned.csv')
+offM = pd.read_csv('nfl_package/datasets/min_offensive_game_stats_cleaned.csv')
+offD = pd.read_csv('nfl_package/datasets/min_opp_game_stats_cleaned.csv')
 st.sidebar.header("Filter Data Here")
 tab1, tab2 = st.tabs(["Offense", "Defense"])
